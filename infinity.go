b5e727746ffc614518086a62ebd3b41bf2b0750c
berquerant <sin@sin-pro.local>
2024-10-31 22:26:33 +0900

    Add stat to Infinity

--- infinity.go
+++ infinity.go
@@ -5,6 +5,7 @@ import "sync"
 // Infinity is an unlimited cache.
 type Infinity[K comparable, V any] struct {
 	sync.Mutex
+	Stat
 	db     map[K]V
 	source Source[K, V]
 }
@@ -21,13 +22,18 @@ func (f *Infinity[K, V]) Get(key K) (V, error) {
 	defer f.Unlock()
 
 	if v, ok := f.db[key]; ok {
+		f.hit++
 		return v, nil
 	}
+	f.miss++
+
 	value, err := f.source(key)
 	if err != nil {
 		var v V
 		return v, err
 	}
+
+	f.size++
 	f.db[key] = value
 	return value, nil
 }
